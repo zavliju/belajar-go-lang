1ecca4ba1f91cff1900bad8590e52a5484bfc19d
zetjede7 <zavli.juwantara7@rocketmail.com>
2018-02-18 06:43:32 +0700

    inisiasi

--- ann.go
+++ ann.go
@@ -15,3 +15,24 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
+// nNet menyimpan semua informasi yang mendefinisikan jaringan syaraf yang sudah dilatih. 
+type nNet struct {
+	config 	nNetConf
+	wHide	*mat.Dense
+	bHide	*mat.Dense
+	wOut	*mat.Dense
+	bOut	*mat.Dense
+}
+
+//nNetConf mendefinisikan arsitektur dan parameter pembelajaran dari jaringan syaraf yang kita miliki.
+type nNetConf struct {
+	inputSyaraf 	int
+	outputSyaraf 	int
+	hiddenSyaraf 	int
+	jumEpoch		int
+	rateLearning	float64
+}
+
+func main() {
+
+}
