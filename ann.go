4bcd4243b8b69b794d67599251551b43f51c4b5c
zetjede7 <zavli.juwantara7@rocketmail.com>
2018-02-17 13:37:58 +0700

    first commit

---
+++ ann.go
@@ -0,0 +1,17 @@
+package ann
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"gonum.org/v1/gonum/floats"
+	"gonum.org/v1/gonum/mat"
+)
+
